7e7f37248aa51621716f921569f5c783fdec9fce
Blake <blakezonca@gmail.com>
2018-08-14 16:00:06 -0500

    Add basic JSON tests, and pull token and prefix from config.json file

--- RambleBot.py
+++ RambleBot.py
@@ -1,11 +1,14 @@
 # Invite link:  https://discordapp.com/oauth2/authorize?client_id=478762645428633612&scope=bot
-
 import discord
 from discord.ext import commands
+import json
 
-TOKEN = ''
+#Load config data, set token, and set prefix
+with open("config.json", "r") as config_file:
+    data = json.load(config_file)
+    client = commands.Bot(command_prefix = data["prefix"])
+    TOKEN = data["token"]
 
-client = commands.Bot(command_prefix = '!')
 client.remove_command('help')
 
 @client.event
@@ -15,8 +18,32 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-# client.say doesn't need to specify a channel like send_message,
-# it simply sends it in the same channel as the command
+#Test JSON by printing out config
+@client.command()
+async def testJSON():
+    with open("config.json", "r") as config_file:
+        #data is dictionary
+        data = json.load(config_file)
+        prefix = data["prefix"]
+        token = data["token"]
+        print("Object type of data : " + str(type(data)))
+    print("Prefix : " + prefix)
+    print("Token : " + token)
+
+@client.command()
+async def testWrite():
+
+    # Convert JSON data from file to dictionary
+    with open("config.json", "r") as config_file:
+        data = json.load(config_file)
+
+    #Add/Remove/Modify data
+    data['New Key'] = 'New value'
+
+    #Write data back to file
+    with open('config.json', 'w') as config_file:
+        # Write dictionary data to output file (config.json)
+        json.dump(data, config_file, indent=4, sort_keys=True)
 
 @client.command()
 async def sub(name):
@@ -34,6 +61,7 @@ async def echo(*args):
     for word in args:
         output += word
         output += ' '
+
     await client.say(output)
 
 @client.command()
@@ -41,7 +69,7 @@ async def help():
 
     embed = discord.Embed(title=" ", color=0xff8233)
     embed.set_author(name="Ramble Bot Help ")
-    embed.add_field(name='!help', value='Shows this help menu', inline=False)
+    embed.add_field(name= client.command_prefix + 'help', value='Shows this help menu', inline=False)
     await client.say(embed=embed)
 
 client.run(TOKEN)

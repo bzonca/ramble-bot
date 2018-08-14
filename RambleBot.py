26f12c611a7ea15b8c58283a07b3463bc634acee
Blake <blakezonca@gmail.com>
2018-08-14 17:30:04 -0500

    Add set command and give user ability to change settings

--- RambleBot.py
+++ RambleBot.py
@@ -3,6 +3,10 @@ import discord
 from discord.ext import commands
 import json
 
+
+color_help = discord.colour.Colour(0xff8233)
+color_success = discord.colour.Colour(0x33ffbd)
+
 #Load config data, set token, and set prefix
 with open("config.json", "r") as config_file:
     data = json.load(config_file)
@@ -45,6 +49,19 @@ async def testWrite():
         # Write dictionary data to output file (config.json)
         json.dump(data, config_file, indent=4, sort_keys=True)
 
+def writeToConfig(key, value):
+    # Convert JSON data from file to dictionary
+    with open("config.json", "r") as config_file:
+        data = json.load(config_file)
+
+    #Add/Remove/Modify data
+    data[key] = value
+
+    #Write data back to file
+    with open('config.json', 'w') as config_file:
+        # Write dictionary data to output file (config.json)
+        json.dump(data, config_file, indent=4, sort_keys=True)
+
 @client.command()
 async def sub(name):
     output = name;
@@ -64,10 +81,43 @@ async def echo(*args):
 
     await client.say(output)
 
+
+#Commands to give user option to set methods
+@client.command()
+async def set(*args):
+
+    property = ""
+    newValue = ""
+    try:
+        property = args[0]
+        newValue = args[1]
+    except IndexError:
+        embed = discord.Embed(title=" ", color=color_help)
+        embed.set_author(name="Set Help ")
+        embed.add_field(name= client.command_prefix + 'set', value='Shows this help menu', inline=False)
+        embed.add_field(name= client.command_prefix + 'set prefix ______', value='Set the command prefix to something else. It could be a character or word', inline=False)
+        await client.say(embed=embed)
+        pass
+    else:
+        if property == "prefix":
+            print ("Set prefix to " + newValue)
+            client.command_prefix = args[1]
+            writeToConfig("prefix", client.command_prefix)
+            embed=discord.Embed(color=color_success)
+            embed.add_field(
+                name="Command prefix updated to " + client.command_prefix,
+                value="All commands now use " + client.command_prefix + " before them. Type " + client.command_prefix +
+                "help for more info", inline=False
+            )
+            await client.say(embed=embed)
+        else:
+            await client.say("Not a valid property to set")
+        pass
+
 @client.command()
 async def help():
 
-    embed = discord.Embed(title=" ", color=0xff8233)
+    embed = discord.Embed(title=" ", color=color_help)
     embed.set_author(name="Ramble Bot Help ")
     embed.add_field(name= client.command_prefix + 'help', value='Shows this help menu', inline=False)
     await client.say(embed=embed)
---
+++ TODO.md
@@ -0,0 +1,5 @@
+## To Do ##
+- Set up twitter authentication
+- Make test calls to api
+- Hook up test calls to dc commands
+- Make JSON file with followed accounts
